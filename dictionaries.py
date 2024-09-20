c787d1323e2148a9de044c3ff087d5e0371d2ac8
Laraib Syed <92629797+laraibsyed@users.noreply.github.com>
2024-09-20 22:07:34 +0400

    directories

---
+++ dictionaries.py
@@ -0,0 +1,8 @@
+soundtrack_dic = {"The Bodyguard":"1992", "Saturday Night Fever":"1977"}
+print(soundtrack_dic.keys())
+print(soundtrack_dic.values())
+
+album_sales_dict = {"Back in Black":"50", "The Bodyguard":"50", "Thriller":"65"}
+print(album_sales_dict["Thriller"])
+print(album_sales_dict.keys())
+print(album_sales_dict.values())

58fa363b255f362d69f61428a27565c1b1f0a4ba
Laraib Syed <92629797+laraibsyed@users.noreply.github.com>
2024-09-27 21:31:58 +0400

    writing files and excerise

---
+++ WritingFiles.py
@@ -0,0 +1,35 @@
+file = "Example2.txt"
+
+with open(file, 'w') as writefile:
+    writefile.write("This is line A\n")
+    writefile.write("This is line B\n")
+
+with open(file, "r") as testwrite:
+    print(testwrite.read())
+
+with open('Example2.txt', 'a+') as testwritefile:
+    print("Initial Location: {}".format(testwritefile.tell()))
+    
+    data = testwritefile.read()
+    if (not data):  #empty strings return false in python
+            print('Read nothing') 
+    else: 
+            print(testwritefile.read())
+            
+    testwritefile.seek(0,0) # move 0 bytes from beginning.
+    
+    print("\nNew Location : {}".format(testwritefile.tell()))
+    data = testwritefile.read()
+    if (not data): 
+            print('Read nothing') 
+    else: 
+            print(data)
+    
+    print("Location after read: {}".format(testwritefile.tell()) )
+
+# Copy file to another
+
+with open('Example2.txt','r') as readfile:
+    with open('Example3.txt','w') as writefile:
+          for line in readfile:
+                writefile.write(line)
